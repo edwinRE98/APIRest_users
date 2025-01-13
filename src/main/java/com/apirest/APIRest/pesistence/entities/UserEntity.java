88e6543ff9c17b412a1f20381dcc3cee8a1f466f
Edwin Ropain E. <edwinropain260@outlook.com>
2025-01-12 21:10:05 -0500

    Changing entity column properties

--- src/main/java/com/apirest/APIRest/pesistence/entities/UserEntity.java
+++ src/main/java/com/apirest/APIRest/pesistence/entities/UserEntity.java
@@ -12,25 +12,24 @@ public class UserEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
     private Integer id;
 
-    @Column(name = "name")
     private String name;
-
     @Column(name = "last_name")
-    private String lastName;
 
+    private String lastName;
     @Column(name = "id_document")
+
     private String idDocument;
 
-    @Column(name = "address")
     private String address;
 
     @Column(name = "phone_number")
+
     private String phoneNumber;
 
     @Column(name = "email")
+
     private String email;
 
     //Getters and Setters
